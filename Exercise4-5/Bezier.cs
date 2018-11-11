2761be738ff98724de497713c9f66c48da434092
DV1X3R <d.purvinsh@gmail.com>
2018-11-12 01:30:14 +0200

    GUI Version init

---
+++ Exercise4-5/Bezier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4_5
+{
+    static class Bezier
+    {
+        public static double Approx(int n, double t, List<double> x)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                sum += x[i] * F(i, n, t);
+            }
+
+            return sum;
+        }
+
+        public static double F(int i, int n, double t)
+        {
+            return C(i, n) * Math.Pow(t, i) * Math.Pow(1 - t, n - i);
+        }
+
+
+        public static double C(int k, int n)
+        {
+            return (Factorial(n) / (Factorial(k) * Factorial(n - k)));
+        }
+
+
+        public static int Factorial(int i)
+        {
+            if (i <= 1)
+                return 1;
+            return i * Factorial(i - 1);
+        }
+    }
+}
---
+++ Exercise4-5/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise4_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            var initXs = new List<double> { -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2 };
+            var initYs = initXs.Select(x => Math.Sin(x * 5) * Math.Exp(x)).ToList();
+            ShowBezier(initXs, initYs);
+            ShowMNK(initXs, initYs);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var initXs = new List<double> { -3.2, -2.1, 0.4, 0.7, 2, 2.5, 2.777 };
+            var initYs = new List<double> { 10, -2, 0, -7, 7, 0, 0 };
+            ShowBezier(initXs, initYs);
+            ShowMNK(initXs, initYs);
+        }
+
+        public void ShowBezier(List<double> initXs, List<double> initYs)
+        {
+            chart1.Series[0].Points.Clear();
+            var resXs = new List<double>();
+            var resYs = new List<double>();
+
+            for (double i = 0; i < 1; i += (1.0F / 30))
+            {
+                var resX = Bezier.Approx(initXs.Count - 1, i, initXs);
+                var resY = Bezier.Approx(initYs.Count - 1, i, initYs);
+
+                resXs.Add(resX);
+                resYs.Add(resY);
+            }
+
+            for (int i = 0; i < resXs.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(resXs[i], resYs[i]);
+            }
+        }
+
+        public void ShowMNK(List<double> initXs, List<double> initYs)
+        {
+            chart1.Series[1].Points.Clear();
+            var resXs = new List<double>();
+            var resYs = new List<double>();
+
+            // TODO: FILL RES
+
+            for (int i = 0; i < resXs.Count; i++)
+            {
+                chart1.Series[1].Points.AddXY(resXs[i], resYs[i]);
+            }
+        }
+
+    }
+}
